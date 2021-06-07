ac87d46102a02d9667991b3e1f0c3427ab2d0325
minzyk <minji121534@gmail.com>
2021-06-07 14:33:51 +0900

    210607 text 입력받기

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from accountapp.models import HelloWorld
 
 # Create your views here.
 # 원하는 뷰 생성 - 브라우저에서 접근하여 Hello World 생성
@@ -8,7 +9,14 @@ from django.shortcuts import render
 def hello_world(request):
 
     if request.method == "POST":
-        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD'})    # HttpResponse ('Hello world!')
+
+        temp = request.POST.get('hello_world_input')       # POST라는 메소드의 hello_world_input 이름을 가진 데이터 요청
+
+        new_hello_world = hello_world()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})    # HttpResponse ('Hello world!') / new_hello_world 로 내보내는 것
     else:
         return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD'})
 
