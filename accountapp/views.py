4ec7cbd0ba5f6901f668c71632a633a60f7b8d56
minzyk <minji121534@gmail.com>
2021-06-07 16:48:58 +0900

    210607 InputText 및 list

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 # Create your views here.
@@ -12,11 +14,14 @@ def hello_world(request):
 
         temp = request.POST.get('hello_world_input')       # POST라는 메소드의 hello_world_input 이름을 가진 데이터 요청
 
-        new_hello_world = hello_world()
+        new_hello_world = HelloWorld()
         new_hello_world.text = temp
         new_hello_world.save()
-
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})    # HttpResponse ('Hello world!') / new_hello_world 로 내보내는 것
+        
+        hello_world_list = HelloWorld.objects.all()      # HelloWorld의 모든 객체들을 hello_world_list에 넣어준다
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))        # reverse 함수를 이용해서 accountapp 내부의 hello_world 에 재접속하도록 만든다 (import 필요)
+        # return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})    # HttpResponse ('Hello world!') / new_hello_world 로 내보내는 것
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
